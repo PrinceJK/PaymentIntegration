7f77b025bbecb0459212e087301586c659c3367e
John Kehinde <kehindejohn018@gmail.com>
2022-01-09 09:32:51 +0100

    Added status to transaction object

---
+++ PaymentIntegration/Migrations/20220109082340_Added status to transaction object.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentIntegration.Migrations
+{
+    public partial class Addedstatustotransactionobject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Transactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Transactions");
+        }
+    }
+}
