41f597308b8f37579de17f24b20bbeff0ba06227
John Kehinde <kehindejohn018@gmail.com>
2022-01-09 09:15:33 +0100

    Configured Paystack

--- PaymentIntegration/Controllers/DonateController.cs
+++ PaymentIntegration/Controllers/DonateController.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PaymentIntegration.Models;
+using PaymentIntegration.Repository;
+using PayStack.Net;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PaymentIntegration.Controllers
 {
     public class DonateController : Controller
     {
-        public DonateController()
+        private readonly IConfiguration _configuration;
+        private readonly AppDbContext _context;
+        private readonly string token;
+        private PayStackApi PayStack { get; set; }
+        public DonateController(IConfiguration configuration,
+            AppDbContext context)
         {
-
+            _configuration = configuration;
+            _context = context;
+            token = _configuration["Payment:PaystackSK"];
+            PayStack = new PayStackApi(token);
         }
 
         [HttpGet]
@@ -17,23 +32,66 @@ namespace PaymentIntegration.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(DonateViewModel donate)
+        public async Task<IActionResult> Index(DonateViewModel donate)
         {
+            TransactionInitializeRequest request = new()
+            {
+                AmountInKobo = donate.Amount * 100,
+                Email = donate.Email,
+                Reference = Generate().ToString(),
+                Currency = "NGN",
+                CallbackUrl = "http://localhost:40352/donate/verify"
+            };
+
+            TransactionInitializeResponse response = PayStack.Transactions.Initialize(request);
+            if (response.Status)
+            {
+                var transaction = new TransactionsModel()
+                {
+                    Amount = donate.Amount,
+                    Email = donate.Email,
+                    TrxRef = request.Reference,
+                    Name = donate.Name,
+                };
+                await _context.Transactions.AddAsync(transaction);
+                await _context.SaveChangesAsync();
+                Redirect(response.Data.AuthorizationUrl);
+            }
+            ViewData["error"] = response.Message;
             return View();
         }
 
         [HttpGet]
-        public IActionResult Donations()
+        public async Task<IActionResult> Donations()
         {
+            var transactions = await _context.Transactions.Where(x => x.Status == true).ToListAsync();
+            ViewData["transactionsList"] = transactions;
             return View();
         }
 
         [HttpGet]
-        public IActionResult Verify()
+        public async Task<IActionResult> Verify(string reference)
         {
-            return View();
+            TransactionVerifyResponse response = PayStack.Transactions.Verify(reference);
+            if (response.Data.Status == "success")
+            {
+                var transaction =  _context.Transactions.Where(x=>x.TrxRef == reference).FirstOrDefault();
+                if (transaction != null)
+                {
+                    transaction.Status = true;
+                    _context.Transactions.Update(transaction);
+                    await _context.SaveChangesAsync(); 
+                    return RedirectToAction("Donations");
+                }
+            }
+            ViewData["error"] = response.Data.GatewayResponse;
+            return RedirectToAction("Index");
         }
 
-        
+        public static int Generate()
+        {
+            Random rand = new Random((int)DateTime.Now.Ticks);
+            return rand.Next(100000000, 999999999);
+        }
     }
 }
